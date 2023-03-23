65c4f435551180fb3f3ddd8ce570af69ae48b36d
harrysudana <harrysudana@gmail.com>
2023-03-23 18:47:38 +0800

    route versioning

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+/**
+ * Version 1
+ */
 
-require __DIR__.'/auth.php';
+Route::prefix('v1')->as ('v1:')->group(
+    base_path('routes/v1/api.php')
+);
